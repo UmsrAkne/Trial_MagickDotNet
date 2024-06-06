7995c44b62c0c177edeaa76fe5fc877ea0b13f35
Akn <sibata486@gmail.com>
2024-06-06 16:29:59 +0900

    init

---
+++ Trial_MagickDotNet/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using Prism.Ioc;
+using Trial_MagickDotNet.Views;
+
+namespace Trial_MagickDotNet
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
---
+++ Trial_MagickDotNet/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Mvvm;
+
+namespace Trial_MagickDotNet.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "Prism Application";
+
+        public string Title { get => title; set => SetProperty(ref title, value); }
+
+        public MainWindowViewModel()
+        {
+        }
+    }
+}
